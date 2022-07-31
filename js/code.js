85054600f32b5a4c8991fd2b80a75af45723feeb
denis-afimin <102680032+denis-afimin@users.noreply.github.com>
2022-07-31 13:59:53 +0300

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,8 +1,8 @@
 document.getElementById('en').onclick = function() {
 	document.getElementById('en').classList.add('activ');
-	document.getElementById('ru').classList.remove('activ');
+	document.getElementById('uk').classList.remove('activ');
 
-	for (let item of document.getElementsByClassName('ru')) {
+	for (let item of document.getElementsByClassName('uk')) {
 		item.classList.add('delete');
 	}
 
@@ -11,15 +11,15 @@ document.getElementById('en').onclick = function() {
 	}
 };
 
-document.getElementById('ru').onclick = function() {
-	document.getElementById('ru').classList.add('activ');
+document.getElementById('uk').onclick = function() {
+	document.getElementById('uk').classList.add('activ');
 	document.getElementById('en').classList.remove('activ');
 
 	for (let item of document.getElementsByClassName('en')) {
 		item.classList.add('delete');
 	}
 
-	for (let item of document.getElementsByClassName('ru')) {
+	for (let item of document.getElementsByClassName('uk')) {
 		item.classList.remove('delete');
 	}
 };
